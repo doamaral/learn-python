ad5b9cc7c69be5fa34988521cba505f782fa3af3
Lucas Amaral <lopesdoamaral@gmail.com>
2015-03-02 14:05:38 -0300

    Alterações pequenas: Tipos de ariáveis

---
+++ level_1/variable_types.py
@@ -0,0 +1,22 @@
+__author__ = 'Usuário'
+
+print("Convertendo um real para inteiro: int(3.14) = ", int(3.14))
+
+print("Convertendo um real para inteiro: float(5) = ", float(5))
+
+print("Operações Real x Inteiro = Real: 5.0/2 + 3 = ", 5.0/2+3)
+
+my_string = "palavra"
+
+print("a "+ my_string + " é string")
+
+print("A string '%s' tem tamanho %d" % (my_string, len(my_string)))
+
+for char in my_string:
+    print(char)
+
+if my_string.startswith('p'):
+    print(my_string.upper())
+
+print(3*my_string)
+
