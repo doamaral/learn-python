22e15737509289a16dc19fbd386f2c2c88883a28
Lucas Amaral <lopesdoamaral@gmail.com>
2015-03-02 23:23:28 -0300

    Alteração do main para exibir melhor os vetores ordenados por cada método

--- level_1/main.py
+++ level_1/main.py
@@ -3,9 +3,16 @@ __author__ = 'Lucas'
 import sort_methods
 
 vetor = [10, 1, 3, 2, 15, 0, 13]
-
-a = sort_methods.insertion(vetor)
-
+a = sort_methods.selection(vetor)
+print("Selection: ", end="")
 print(a)
 
-#print(b)
+vetor = [10, 1, 3, 2, 15, 0, 13]
+a = sort_methods.insertion(vetor)
+print("Insertion: ", end="")
+print(a)
+
+vetor = [10, 1, 3, 2, 15, 0, 13]
+a = sort_methods.bubble(vetor)
+print("Bubble:", end="")
+print(a)
